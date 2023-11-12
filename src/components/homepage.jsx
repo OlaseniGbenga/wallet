a7b941397a0024d677ee499eae6c730e4806d369
Papi <gj.olaseni@gmail.com>
2023-11-12 10:18:32 +0100

    main branch

---
+++ src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className='flex flex-cols px-24 sm:px-1'>
+        <div>
+<p>Monitor Balance</p>
+<p>Control Smart Life</p>
+        </div>
+        <div>
+            <img src="Banner.png" alt="" />
+        </div>
+    </div>
+  )
+}
+ 
